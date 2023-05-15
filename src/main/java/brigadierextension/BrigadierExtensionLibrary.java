0f49a561c50c0d0cbd9cd5c2fc61b09dae0a8365
Redstonia <redstonia1234@gmail.com>
2023-05-15 18:08:35 +1200

    actual initial commit

--- src/main/java/brigadierextension/BrigadierExtensionLibrary.java
+++ src/main/java/brigadierextension/BrigadierExtensionLibrary.java
@@ -1,11 +1,11 @@
-package template;
+package brigadierextension;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TemplateMod implements ModInitializer {
-	public static final String MOD_ID = "";
+public class BrigadierExtensionLibrary implements ModInitializer {
+	public static final String MOD_ID = "brigadier-extension-library";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
