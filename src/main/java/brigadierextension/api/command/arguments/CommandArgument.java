9887d03a0690978f4ba0e5f5a72338e9e7a7c0a3
Redstonia <redstonia1234@gmail.com>
2023-05-17 17:33:29 +1200

    passing CommandArgument into ArgumentBuilder.then() through pass()

--- src/main/java/brigadierextension/api/command/arguments/CommandArgument.java
+++ src/main/java/brigadierextension/api/command/arguments/CommandArgument.java
@@ -1,21 +1,42 @@
 package brigadierextension.api.command.arguments;
 
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.command.ServerCommandSource;
 
 public class CommandArgument<T> {
     private final String identifier;
-    private final ArgumentType<T> argumentInputType;
-    private final Class<T> argumentOutputType;
+    private final ArgumentType<T> argumentType;
+    private final Class<T> outputType;
 
-    public CommandArgument(String identifier, ArgumentType<T> argumentInputType, Class<T> argumentOutputType) {
+    /**
+     * Creates a new CommandArgument.
+     * @param identifier   The identifier of the argument. Think of this as the "name" of the argument.
+     * @param argumentType The type of argument this is. This will be how values are passed to the argument in-game. For
+     *                     example, {@link StringArgumentType#greedyString() greedyString()} or
+     *                     {@link IntegerArgumentType#integer() integer()}.
+     * @param outputType   What you get from the argument. For example, {@code String.class} if you get a string from
+     *                     the command argument.
+     */
+    public CommandArgument(String identifier, ArgumentType<T> argumentType, Class<T> outputType) {
         this.identifier = identifier;
-        this.argumentInputType = argumentInputType;
-        this.argumentOutputType = argumentOutputType;
+        this.argumentType = argumentType;
+        this.outputType = outputType;
+    }
+
+    /**
+     * Gives you useful stuff.
+     * @return A RequiredArgumentBuilder that can be simply passed into {@link ArgumentBuilder#then}.
+     */
+    public <S> RequiredArgumentBuilder<S, T> pass() {
+        return RequiredArgumentBuilder.argument(identifier, argumentType);
     }
 
     public T get(CommandContext<ServerCommandSource> context) {
-        return context.getArgument(identifier, argumentOutputType);
+        return context.getArgument(identifier, outputType);
     }
 }
