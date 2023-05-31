5d4283c993121bb559a20163c3d1b407231050d9
Redstonia <redstonia1234@gmail.com>
2023-05-31 17:01:27 +1200

    commandArgument can have suggestionProvider now

--- src/main/java/brigadierextension/api/arguments/CommandArgument.java
+++ src/main/java/brigadierextension/api/arguments/CommandArgument.java
@@ -3,21 +3,28 @@ package brigadierextension.api.arguments;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 
 import static brigadierextension.api.simplecommands.providers.UniversalCommandContextProvider.*;
 
-public class CommandArgument<T> {
+public class CommandArgument<S, T> {
     private final String identifier;
     private final ArgumentType<T> argumentType;
     private final Class<T> outputType;
+    private final SuggestionProvider<S> suggestionProvider;
 
     public CommandArgument(String identifier, ArgumentType<T> argumentType, Class<T> outputType) {
+        this(identifier, argumentType, outputType, null);
+    }
+
+    public CommandArgument(String identifier, ArgumentType<T> argumentType, Class<T> outputType, SuggestionProvider<S> suggestionProvider) {
         this.identifier = identifier;
         this.argumentType = argumentType;
         this.outputType = outputType;
+        this.suggestionProvider = suggestionProvider;
     }
 
-    public <S> RequiredArgumentBuilder<S, T> pass() {
+    public RequiredArgumentBuilder<S, T> pass() {
         return RequiredArgumentBuilder.argument(identifier, argumentType);
     }
 
@@ -25,7 +32,7 @@ public class CommandArgument<T> {
         return get(context);
     }
 
-    public <S> T get(CommandContext<S> ctx) {
+    public T get(CommandContext<?> ctx) {
         return ctx.getArgument(identifier, outputType);
     }
 }
