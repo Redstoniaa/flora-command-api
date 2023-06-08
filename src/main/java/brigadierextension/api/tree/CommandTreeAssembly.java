33f3de70f846fb6e18d3d23579dc9ec6b07cd9b5
Redstonia <redstonia1234@gmail.com>
2023-06-08 17:53:52 +1200

    make sorting work properly- hopefully

--- src/main/java/brigadierextension/api/tree/CommandTreeAssembly.java
+++ src/main/java/brigadierextension/api/tree/CommandTreeAssembly.java
@@ -10,14 +10,20 @@ import java.util.Map;
 public class CommandTreeAssembly {
     public static <S> CommandNode<S> build(TreeBuilder<S, ?> rootBuilder) {
         List<TreeBuilder<S, ?>> floatingBuilders = rootBuilder.collectChildren();
-        floatingBuilders.sort((a, b) -> Boolean.compare(a.redirectionKey == null, b.redirectionKey == null));
+        redirectKeysFirst(floatingBuilders);
 
         Map<TreeBuilder<S, ?>, CommandNode<S>> blueprint = new HashMap<>();
-
         for (TreeBuilder<S, ?> treeBuilder : floatingBuilders) {
             CommandNode<S> node = treeBuilder.build();
             blueprint.put(treeBuilder, node);
             // handle redirect logic
         } return rootBuilder.assemble(blueprint);
+    }
+
+    /**
+     * Sort {@link List} of {@link TreeBuilder}s, with the ones with redirect keys first.
+     */
+    public static <S> void redirectKeysFirst(List<TreeBuilder<S, ?>> list) {
+        list.sort((a,b) -> Boolean.compare(a.redirectionKey != null, b.redirectionKey != null));
     }
 }
