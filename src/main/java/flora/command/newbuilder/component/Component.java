a424460c408d8a2fdc2ce624defb949c908a0250
Redstonia <redstonia1234@gmail.com>
2023-07-26 17:24:58 +1200

    Continue laying groundwork for components.

--- src/main/java/flora/command/newbuilder/component/Component.java
+++ src/main/java/flora/command/newbuilder/component/Component.java
@@ -1,14 +1,28 @@
 package flora.command.newbuilder.component;
 
+import flora.command.builder.CommandBuildInfo;
+
 /**
- * A component of a TreeBuilder.
+ * A component of a NodeBuilder.
  *
- * @param <T> The type that is outputted by this component that is understood by Brigadier.
+ * @param <S> The source type of the builder.
+ * @param <V> The input value held by this component.
+ * @param <R> The type that is outputted by this component that is understood by Brigadier.
  */
-public interface Component<T> {
+public class Component<S, V, R> {
+    V value;
+    ComponentFunction<S, V, R> function;
+    
+    public Component(V value, ComponentFunction<S, V, R> function) {
+        this.value = value;
+        this.function = function;
+    }
+    
     /**
      * @return A representation of the information held in this component in a type that can be interpreted by
      * Brigadier.
      */
-    T get();
+    R get(CommandBuildInfo<S> info) {
+        return function.apply(value, info);
+    }
 }
---
+++ src/main/java/flora/command/newbuilder/component/ComponentFunction.java
@@ -0,0 +1,17 @@
+package flora.command.newbuilder.component;
+
+import flora.command.builder.CommandBuildInfo;
+
+import java.util.function.BiFunction;
+
+/**
+ * A function of a component of a NodeBuilder that takes a value of a given type and returns a value that can be
+ * understood by Brigadier.
+ *
+ * @param <S> The command source type of the builder.
+ * @param <V> The type of the input value.
+ * @param <R> The return type, which can be interpreted by Brigadier.
+ */
+@FunctionalInterface
+public interface ComponentFunction<S, V, R>
+        extends BiFunction<V, CommandBuildInfo<S>, R> {}
