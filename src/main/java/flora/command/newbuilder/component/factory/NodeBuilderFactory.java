22b0eab896646743a7f32870b7f23974f5eddc1d
Redstonia <redstonia1234@gmail.com>
2023-07-31 16:54:01 +1200

    @SafeVarargs on lit()

--- src/main/java/flora/command/newbuilder/component/factory/NodeBuilderFactory.java
+++ src/main/java/flora/command/newbuilder/component/factory/NodeBuilderFactory.java
@@ -7,6 +7,7 @@ import flora.command.newbuilder.component.applier.ComponentApplier;
 import static flora.command.newbuilder.component.factory.CentralComponentFactory.asIsFunction;
 
 public class NodeBuilderFactory {
+    @SafeVarargs
     public static <S> LiteralNodeBuilder<S> lit(final String literal, ComponentApplier<S, ? super LiteralNodeBuilder<S>>... appliers) {
         LiteralNodeBuilder<S> builder = new LiteralNodeBuilder<>();
         builder.literal = new LiteralComponent<>(literal, asIsFunction());
