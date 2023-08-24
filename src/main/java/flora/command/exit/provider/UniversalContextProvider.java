810d128dbe0ad18c9fea8d6e0d9553795e2c39f3
Redstonia <redstonia1234@gmail.com>
2023-08-24 17:55:56 +1200

    Command executions, to fully replace context providers - which really sucked, and I'm glad to see them go!

--- src/main/java/flora/command/exit/provider/UniversalContextProvider.java
+++
@@ -1,7 +0,0 @@
-package flora.command.exit.provider;
-
-import com.mojang.brigadier.context.CommandContext;
-
-public class UniversalContextProvider {
-    public static CommandContext<?> context;
-}
